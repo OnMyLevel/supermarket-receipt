925406d7e4dfc14e94a0b881cece1d370c8d0985
christanismerilbanzouzi <merilb78@gmail.com>
2019-01-15 15:50:14 +0100

    remplacement de la dépendance JUnit3 par JUnit5,ajouter la dépendance vers la librairie d'assertion assertj-core,remplacement de l'héritage de la classe TestCase par l'annotation @Test et utilisation l'API d'AssertJ pour la première assertion

--- src/test/java/fr/esiea/AppTest.java
+++ src/test/java/fr/esiea/AppTest.java
@@ -1,8 +1,9 @@
 package fr.esiea;
 
-import static org.junit.Assert.assertTrue;
-
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
@@ -15,6 +16,6 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        Assertions.assertThat(true).isTrue();
     }
 }
