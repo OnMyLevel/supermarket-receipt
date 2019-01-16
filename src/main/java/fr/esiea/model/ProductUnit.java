e76ec6a57f6e95c371a2e11228e5a2b4df75274f
christanismerilbanzouzi <merilb78@gmail.com>
2019-01-16 14:10:38 +0100

    Récupération du code initial du kata SupermarketReceipt

---
+++ src/main/java/fr/esiea/model/ProductUnit.java
@@ -0,0 +1,5 @@
+package fr.esiea.model;
+
+public enum ProductUnit {
+    Kilo, Each
+}
---
+++ src/main/java/fr/esiea/model/SupermarketCatalog.java
@@ -0,0 +1,8 @@
+package fr.esiea.model;
+
+public interface SupermarketCatalog {
+    void addProduct(Product product, double price);
+
+    double getUnitPrice(Product product);
+
+}
