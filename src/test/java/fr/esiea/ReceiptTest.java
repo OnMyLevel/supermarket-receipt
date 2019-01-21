40cd3da09663afab01e2a6e31ec3fbebb9fbcf98
christanismerilbanzouzi <merilb78@gmail.com>
2019-01-21 11:42:31 +0100

    creation et ajout de la class test ReceiptTest

---
+++ src/test/java/fr/esiea/ReceiptTest.java
@@ -0,0 +1,68 @@
+package fr.esiea;
+
+import fr.esiea.model.*;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class ReceiptTest {
+
+    @Test
+    public void receiptPriceTest() {
+        SupermarketCatalog catalog = new FakeCatalog();
+        Product toothbrush = new Product("toothbrush", ProductUnit.Each);
+        catalog.addProduct(toothbrush, 0.99);
+        Product apples = new Product("apples", ProductUnit.Kilo);
+        catalog.addProduct(apples, 1.99);
+
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItemQuantity(apples, 2.5);
+
+        Teller teller = new Teller(catalog);
+        teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, 10.0);
+
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+
+        Assertions.assertThat("4.975").isEqualTo(receipt.getTotalPrice().toString());
+        // Todo: complete this test
+    }
+
+    @Test
+    public void receiptSiseDiscountTest() {
+        SupermarketCatalog catalog = new FakeCatalog();
+        Product toothbrush = new Product("toothbrush", ProductUnit.Each);
+        catalog.addProduct(toothbrush, 0.99);
+        Product apples = new Product("apples", ProductUnit.Kilo);
+        catalog.addProduct(apples, 1.99);
+
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItemQuantity(apples, 2.5);
+
+        Teller teller = new Teller(catalog);
+        teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, 10.0);
+
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+
+        Assertions.assertThat(0).isEqualTo(receipt.getDiscounts().size());
+        // Todo: complete this test
+    }
+
+    @Test
+    public void receiptNbitemsTest() {
+        SupermarketCatalog catalog = new FakeCatalog();
+        Product toothbrush = new Product("toothbrush", ProductUnit.Each);
+        catalog.addProduct(toothbrush, 0.99);
+        Product apples = new Product("apples", ProductUnit.Kilo);
+        catalog.addProduct(apples, 1.99);
+
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItemQuantity(apples, 2.5);
+
+        Teller teller = new Teller(catalog);
+        teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, 10.0);
+
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+
+        Assertions.assertThat(1).isEqualTo(receipt.getItems().size());
+        // Todo: complete this test
+    }
+}
