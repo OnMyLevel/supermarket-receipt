7cb222b4cc0b4423b9f5e0277c18733a9e9be50a
christanismerilbanzouzi <merilb78@gmail.com>
2019-01-21 11:44:21 +0100

    creation et ajout de la class test ReceiptTest

--- src/test/java/fr/esiea/AppTest.java
+++ src/test/java/fr/esiea/AppTest.java
@@ -1,10 +1,7 @@
 package fr.esiea;
 
-import fr.esiea.model.*;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
-
-import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
@@ -19,26 +16,5 @@ public class AppTest
     {
         Assertions.assertThat(true).isTrue();
 
-    }
-    @Test
-    public void appleTest() {
-        SupermarketCatalog catalog = new FakeCatalog();
-        Product toothbrush = new Product("toothbrush", ProductUnit.Each);
-        catalog.addProduct(toothbrush, 0.99);
-        Product apples = new Product("apples", ProductUnit.Kilo);
-        catalog.addProduct(apples, 1.99);
-
-        ShoppingCart cart = new ShoppingCart();
-        cart.addItemQuantity(apples, 2.5);
-
-        Teller teller = new Teller(catalog);
-        teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, 10.0);
-
-        Receipt receipt = teller.checksOutArticlesFrom(cart);
-        
-        Assertions.assertThat("4.975").isEqualTo(receipt.getTotalPrice().toString());
-        Assertions.assertThat(0).isEqualTo(receipt.getDiscounts().size());
-        Assertions.assertThat(1).isEqualTo(receipt.getItems().size());
-    // Todo: complete this test
     }
 }
