c5247e86bc6bb2219efc9ed6a1ba08473c23bd8b
Amineesiea <azlouk@et.esiea.fr>
2019-01-22 11:17:26 +0100

    Ajout de tests sur receiptPrinter

--- src/test/java/fr/esiea/ReceiptPrinterTest.java
+++ src/test/java/fr/esiea/ReceiptPrinterTest.java
@@ -13,13 +13,17 @@ public class ReceiptPrinterTest {
 
         SupermarketCatalog catalog = new FakeCatalog();
         Product rasoir = new Product("Rasoir", ProductUnit.Each);
+        Product salade = new Product("Salade", ProductUnit.Kilo);
         catalog.addProduct(rasoir, 1.50);
+        catalog.addProduct(salade, 0.50);
 
         ShoppingCart cart = new ShoppingCart();
         cart.addItemQuantity(rasoir, 3);
+        cart.addItemQuantity(salade, 1);
 
         Teller teller = new Teller(catalog);
         teller.addSpecialOffer(SpecialOfferType.ThreeForTwo, rasoir, 1);
+        teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, salade, 1);
 
         Receipt receipt = teller.checksOutArticlesFrom(cart);
 
@@ -28,9 +32,11 @@ public class ReceiptPrinterTest {
         String expectedPrint =
             "Rasoir                              4.50\n"
                 +"  1.50 * 3\n"
+                +"Salade                              0.50\n"
                 +"3 for 2(Rasoir)                    -1.50\n"
+                +"1.0% off(Salade)                   -0.01\n"
                 +"\n"
-                +"Total:                              3.00";
+                +"Total:                              3.50";
 
         Assertions.assertThat(expectedPrint).isEqualTo(printerDefault.printReceipt(receipt));
     }
