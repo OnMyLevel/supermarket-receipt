a3350c324ee29886f4fe6f0d666ace88884709cc
ilyess <ilyess94500@gmail.com>
2019-01-23 17:57:49 +0100

    TP2, 100% de la classe ReceiptItem est couvert, ainsi qu'une correction sur les test de ReceiptPrinter a été ajouté

--- src/test/java/fr/esiea/ReceiptItemTest.java
+++ src/test/java/fr/esiea/ReceiptItemTest.java
@@ -28,4 +28,40 @@ public class ReceiptItemTest {
         Assertions.assertThat(p).isEqualTo(ri.getProduct());
 
     }
+
+
+
+    @Test
+    public void getTotalPrice(){
+
+        Product p = new Product("Rasoir", ProductUnit.Each);
+        ReceiptItem ri = new ReceiptItem(p, 2, 4.11, 8.22 );
+        //System.out.println(ri.getPrice());
+        Assertions.assertThat(8.22).isEqualTo(ri.getTotalPrice());
+
+    }
+
+    @Test
+    public void getQuantityTest(){
+
+        Product p = new Product("Rasoir", ProductUnit.Each);
+        ReceiptItem ri = new ReceiptItem(p, 2, 4.11, 8.22 );
+        //System.out.println(ri.getPrice());
+        Assertions.assertThat(2.0).isEqualTo(ri.getQuantity());
+
+    }
+
+    @Test
+    public void CompareReceiptItemTest(){
+        Product p1 = new Product("Rasoir", ProductUnit.Each);
+        Product p2 = new Product("salade", ProductUnit.Each);
+
+
+        ReceiptItem ri = new ReceiptItem(p1,2,4,8);
+        ReceiptItem ri2 = new ReceiptItem(p2,2,4,8);
+
+        Assertions.assertThat(false).isEqualTo(ri.equals(ri2));
+        Assertions.assertThat(ri.hashCode()).isNotEqualTo(ri2.hashCode());
+
+    }
 }
