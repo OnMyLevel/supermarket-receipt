40cf55be9c4532d57a46a4a80fe174b4524f57be
christanismerilbanzouzi <merilb78@gmail.com>
2019-01-23 22:44:18 +0100

    TP2, ajout de nouveau cas de test

--- src/test/java/fr/esiea/ProductTest.java
+++ src/test/java/fr/esiea/ProductTest.java
@@ -23,4 +23,22 @@ public class ProductTest {
         Assertions.assertThat(false).isEqualTo(pain.equals(rasoir));
         Assertions.assertThat(rasoir.hashCode()).isNotEqualTo(pain.hashCode());
     }
+
+    @Test
+    public void  notNullEqualTest(){
+
+        Product rasoir = new Product("Rasoir", ProductUnit.Each);
+       // Product pain ;
+        Assertions.assertThat(false).isEqualTo(rasoir.equals(null));
+        Assertions.assertThat(rasoir.hashCode()).isNotEqualTo(null);
+    }
+
+    @Test
+    public void  notClasslEqualTest(){
+
+        Product rasoir = new Product("Rasoir", ProductUnit.Each);
+         Object pain = new  Product("Rasoir", ProductUnit.Each);;
+        Assertions.assertThat(true).isEqualTo(rasoir.equals(pain));
+        Assertions.assertThat(rasoir.hashCode()).isNotEqualTo(pain);
+    }
 }
